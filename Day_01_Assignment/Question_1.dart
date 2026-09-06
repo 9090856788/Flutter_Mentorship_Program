cc7b317a30f39661e8cad373e7c781a1e2d63df2
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2026-09-06 12:29:00 +0530

    Question 1 completed

---
+++ Day_01_Assignment/Question_1.dart
@@ -0,0 +1,8 @@
+void main(){
+  String fullName = "Kanhu Charan Sahoo";
+  int age = 27;
+  double height = 1.7;
+  bool isStudent = false;
+  print("Name: $fullName, Age: $age");
+  print("Height: $height m, Student: $isStudent");
+}
